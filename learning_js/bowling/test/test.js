ffa63acded786e5b2131484da592b282e2caa329
JFHarris <JFHarris1983@gmail.com>
2023-01-24 23:33:38 -0600

    Added second test/updated bowling---all tests passing

--- learning_js/bowling/test/test.js
+++ learning_js/bowling/test/test.js
@@ -10,3 +10,13 @@ it("should return 0 for a game of only gutter balls", () => {
 
   expect(game.score).to.equal(0);
 });
+
+it("should return 20 for a game where all rolls knock down one pin", () => {
+  const game = new BowlingGame();
+
+  for (let x = 0; x < 20; x++) {
+    game.roll(1);
+  }
+
+  expect(game.score).to.equal(20);
+});
