68a4da201e8cd1db0ad2299e322feb2a44b5af33
JFHarris <JFHarris1983@gmail.com>
2023-01-25 19:12:39 -0600

    First test refactor--added beforeEach and extracted for loop to rollMany function

--- learning_js/bowling/test/test.js
+++ learning_js/bowling/test/test.js
@@ -1,22 +1,26 @@
 import BowlingGame from ".././bowling.js";
 import chai, { expect } from "chai";
 
-it("should return 0 for a game of only gutter balls", () => {
-  const game = new BowlingGame();
+let game;
 
-  for (let x = 0; x < 20; x++) {
-    game.roll(0);
-  }
+beforeEach(() => {
+  game = new BowlingGame();
+});
+
+it("should return 0 for a game of only gutter balls", () => {
+  rollMany(0, 20);
 
   expect(game.score).to.equal(0);
 });
 
 it("should return 20 for a game where all rolls knock down one pin", () => {
-  const game = new BowlingGame();
-
-  for (let x = 0; x < 20; x++) {
-    game.roll(1);
-  }
+  rollMany(1, 20);
 
   expect(game.score).to.equal(20);
 });
+
+function rollMany(pins, rolls) {
+  for (let x = 0; x < rolls; x++) {
+    game.roll(pins);
+  }
+}
