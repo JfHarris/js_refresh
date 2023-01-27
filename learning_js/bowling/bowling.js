19b58b7e37ef13125bb8bb872b5d218c432678f0
JFHarris <JFHarris1983@gmail.com>
2023-01-27 12:21:59 -0600

    Final refactor---added isStrike & strikeBonus---all tests passing

--- learning_js/bowling/bowling.js
+++ learning_js/bowling/bowling.js
@@ -12,8 +12,8 @@ class BowlingGame {
     let rollIndex = 0;
 
     for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
-      if (this.rolls[rollIndex] === 10) {
-        score += 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+      if (this.isStrike(rollIndex)) {
+        score += this.strikeBonus(rollIndex);
         rollIndex++;
         continue;
       }
@@ -37,6 +37,14 @@ class BowlingGame {
 
   spareBonus(rollIndex) {
     return 10 + this.rolls[rollIndex + 2];
+  }
+
+  isStrike(rollIndex) {
+    return this.rolls[rollIndex] === 10;
+  }
+
+  strikeBonus(rollIndex) {
+    return 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
   }
 }
 
