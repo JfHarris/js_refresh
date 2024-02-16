a68de41e549763447ecc3cd08c1ceb5684dc3a44
JFHarris <JFHarris1983@gmail.com>
2024-02-15 22:14:17 -0600

    Added save

--- jsBasics/index.js
+++ jsBasics/index.js
@@ -1,5 +1,6 @@
 let count = 0;
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 function increment() {
   count++;
@@ -7,5 +8,8 @@ function increment() {
 }
 
 function save() {
-  console.log(count);
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  count = 0;
+  countEl.textContent = count;
 }
