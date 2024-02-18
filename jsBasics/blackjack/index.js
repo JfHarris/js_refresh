17678b7bc5f169ad8517b559100a149293bf19f0
JFHarris <JFHarris1983@gmail.com>
2024-02-17 22:58:16 -0600

    Second commit

--- jsBasics/blackjack/index.js
+++ jsBasics/blackjack/index.js
@@ -1,19 +1,64 @@
-let firstCard = 10;
-let secondCard = 11;
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+//let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "Woohoo! You've got Blackjack!";
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+  return randomNumber;
 
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game!";
-  isAlive = false;
 }
 
-console.log(message);
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+function renderGame() {
+
+  cardsEl.textContent = "Cards: ";
+  //cardsEl.textContent = "Cards: " + cards.join(" ");
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum;
+
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  if (isAlive && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
+}
+
+
