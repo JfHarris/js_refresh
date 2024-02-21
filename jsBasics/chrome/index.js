274d34bce8e3a32e51f0dafd466b1439c164b356
JFHarris <JFHarris1983@gmail.com>
2024-02-20 23:37:59 -0600

    Added save tab button/styling

--- jsBasics/chrome/index.js
+++ jsBasics/chrome/index.js
@@ -1,23 +1,24 @@
-let myLeads = [];
+let myBookmarks = [];
 const inputEl = document.getElementById("input-el"); 
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const bookmarksFromLocalStorage = JSON.parse(localStorage.getItem("myBookmarks"));
+const tabBtn = document.getElementById("tab-btn");
 
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  renderLeads();
+if (bookmarksFromLocalStorage) {
+  myBookmarks = bookmarksFromLocalStorage;
+  render(myBookmarks);
 }
 
-function renderLeads() {
+function render(bookmarks) {
   let listItems = "";
 
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < bookmarks.length; i++) {
     listItems += `
     <li> 
-      <a href='${myLeads[i]}' target='_blank'>
-      ${myLeads[i]}
+      <a href='${bookmarks[i]}' target='_blank'>
+      ${bookmarks[i]}
       </a>
     </li>
     `;
@@ -27,17 +28,27 @@ function renderLeads() {
 };
 
 inputBtn.addEventListener("click", function() {
-  myLeads.push(inputEl.value);
+  myBookmarks.push(inputEl.value);
   inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  console.log(localStorage.getItem("myLeads"));
-  renderLeads();
+  localStorage.setItem("myBookmarks", JSON.stringify(myBookmarks));
+  console.log(localStorage.getItem("myBookmarks"));
+  render(myBookmarks);
 });
+
+tabBtn.addEventListener("click", function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myBookmarks.push(tabs[0].url);
+    localStorage.setItem("myBookmarks", JSON.stringify(myBookmarks));
+    render(myBookmarks);
+  })
+
+
+})
 
 deleteBtn.addEventListener("dblclick", function() {
   localStorage.clear();
-  myLeads = [];
-  renderLeads();
+  myBookmarks = [];
+  render(myBookmarks);
 })
 
 
